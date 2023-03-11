cc2955f5eb02233f766b90c33d6ad55366fd6a2c
Samira Samadi <ss5ug@virginia.edu>
2023-03-11 16:08:11 -0500

    Re-implemented and refactored HuntingtonHillApportionmentStrategy from HW 1C

--- src/main/java/edu/virginia/cs/hw3/HuntingtonHillApportionmentStrategy.java
+++ src/main/java/edu/virginia/cs/hw3/HuntingtonHillApportionmentStrategy.java
@@ -3,11 +3,58 @@ package edu.virginia.cs.hw3;
 import java.util.*;
 
 public class HuntingtonHillApportionmentStrategy extends ApportionmentStrategy {
+    private List<State> stateList;
+    private int targetRepresentatives;
+    private Apportionment apportionment;
+
+    // huntingtonHill method is from homework 1C
 
     @Override
     public Apportionment getApportionment(List<State> stateList, int representatives) {
-        return null;
-
-        //TODO: Unimplemented stub
+        initializeFields(stateList, representatives);
+        return getApportionmentIntValue();
     }
+
+    private Apportionment getApportionmentIntValue() {
+        apportionment = assignOneRepresentative();
+        assignRemainingRepsByPriority();
+        return apportionment;
+    }
+
+    private void initializeFields(List<State> stateList, int representatives) {
+        this.stateList = stateList;
+        targetRepresentatives = representatives;
+        apportionment = new Apportionment();
+    }
+    private Apportionment assignOneRepresentative() {
+        for(State state : stateList) {
+            apportionment.addRepresentativesToState(state, 1);
+        }
+        return apportionment;
+    }
+    private void assignRemainingRepsByPriority() {
+        while (getRepsLeftToAssign() > 0)  {
+            State highestPriorityState = getHighestPriority();
+            apportionment.addRepresentativesToState(highestPriorityState, 1);
+        }
+    }
+    private State getHighestPriority() {
+        double highestPriority = 0.0;
+        State highestPriorityState = null;
+        for(State state : stateList) {
+            int n = apportionment.getRepresentativesForState(state);
+            double priority = state.getPopulation() / (Math.sqrt(n * (n + 1)));
+            if (priority > highestPriority) {
+                highestPriorityState = state;
+                highestPriority = priority;
+            }
+        }
+        return highestPriorityState;
+    }
+
+    private int getRepsLeftToAssign() {
+        int assignedRepresentatives = apportionment.getAllocatedRepresentatives();
+        return targetRepresentatives - assignedRepresentatives;
+    }
+
 }
