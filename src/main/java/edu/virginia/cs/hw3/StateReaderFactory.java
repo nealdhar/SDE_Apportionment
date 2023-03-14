cd2784436f85ce75c4348be0402ea62a30eac4f9
nealdhar <nd2pvz@virginia.edu>
2023-03-13 20:01:48 -0400

    Attempting to implement StateReaderFactory

--- src/main/java/edu/virginia/cs/hw3/StateReaderFactory.java
+++ src/main/java/edu/virginia/cs/hw3/StateReaderFactory.java
@@ -1,5 +1,25 @@
 package edu.virginia.cs.hw3;
 
 public class StateReaderFactory {
+    StateReader CSVStateReader ;
+    StateReader XLSXStateReader ;
+
+    StateReader stateReader ;
+
+    private void setStateReaderFromFileName(String fileName) {
+        if (fileName.contains(".csv")) {
+            stateReader = CSVStateReader;
+        }
+        if (fileName.contains(".xlsx")) {
+            stateReader = XLSXStateReader ;
+        }
+    }
+    private static StateReader getStateReader(String filename){
+        StateReaderFactory factory = new StateReaderFactory();
+        StateReader stateReader = factory.getStateReader(filename);
+
+    return stateReader;
+    }
+
 
 }
