ed16aa7e82e9f167b6b2d1085f8cfd2fc173d130
nealdhar <nd2pvz@virginia.edu>
2023-03-14 11:08:34 -0400

    Implementing ApportionmentStrategyFactory class

--- src/main/java/edu/virginia/cs/hw3/ApportionmentStrategyFactory.java
+++ src/main/java/edu/virginia/cs/hw3/ApportionmentStrategyFactory.java
@@ -1,4 +1,35 @@
 package edu.virginia.cs.hw3;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ApportionmentStrategyFactory {
+    private final List<String> arguments;
+    private Configuration config;
+
+    public ApportionmentStrategyFactory(String[] args) {
+        this(Arrays.asList(args));
+    }
+
+    public ApportionmentStrategyFactory(List<String> arguments) {
+        if (arguments.size() < 1) {
+            throw new IllegalArgumentException("Error: No arguments were included at runtime. Arguments expected\n" +
+                    "statePopulationFilename [number of representatives] [--hamilton]");
+        }
+        this.arguments = arguments;
+    }
+    private void setApportionmentStrategy() {
+        if (arguments.contains("hamilton")) {
+            config.setApportionmentStrategy(new HamiltonApportionmentStrategy());
+        }
+        if (arguments.contains("jefferson")) {
+            config.setApportionmentStrategy(new JeffersonApportionmentStrategy());
+        }
+    }
+    public ApportionmentStrategy getApportionmentStrategy() {
+        ApportionmentStrategyFactory factory = new ApportionmentStrategyFactory(arguments);
+        factory.setApportionmentStrategy();
+        ApportionmentStrategy apportionmentStrategy = factory.getApportionmentStrategy();
+        return apportionmentStrategy ;
+    }
 }
