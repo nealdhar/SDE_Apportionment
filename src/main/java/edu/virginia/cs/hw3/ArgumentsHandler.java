ebcccf8b2a0be4b3f46e258884f8c053d9ce9880
Samira Samadi <ss5ug@virginia.edu>
2023-03-19 13:31:29 -0400

    Refactored argument handler -- now it works for all flags

--- src/main/java/edu/virginia/cs/hw3/ArgumentsHandler.java
+++ src/main/java/edu/virginia/cs/hw3/ArgumentsHandler.java
@@ -5,15 +5,17 @@ import java.util.*;
 public class ArgumentsHandler {
 
     public static final int FILENAME_INDEX = 0;
+
     private final List<String> arguments;
-    private Configuration config;
+    private final Configuration config;
 
     public ArgumentsHandler(List<String> arguments) {
-        if (arguments.size() < 1) {
+        if (arguments.isEmpty()) {
             throw new IllegalArgumentException("Error: No arguments were included at runtime. Arguments expected\n" +
                     "statePopulationFilename [number of representatives] [--hamilton]");
         }
         this.arguments = arguments;
+        this.config = new Configuration();
     }
 
     public ArgumentsHandler(String[] args) {
@@ -22,69 +24,85 @@ public class ArgumentsHandler {
 
     public Configuration getConfiguration() {
         setDefaultConfiguration();
+        parseOptionalArguments();
         configureStateReader();
-        readCommandLineFlags();
         return config;
     }
-
     private void setDefaultConfiguration() {
-        config = new Configuration();
         config.setApportionmentStrategy(new HuntingtonHillApportionmentStrategy());
         config.setRepresentatives(435);
         config.setApportionmentFormat(new AlphabeticalApportionmentFormat());
     }
-
-    public void readCommandLineFlags() {
-        for (int i = 0; i < arguments.size(); i++) {
-            String flagged_argument = arguments.get(i + 1);
-
-            if (arguments.get(i).startsWith("--")) {
-                String long_flag = arguments.get(i);
-
-                if (long_flag.equals("--algorithm")) {
-                    String apportionmentName = flagged_argument;
-                    configureStrategy(apportionmentName);
-                }
-                if (long_flag.equals("--format")) {
-                    String format = flagged_argument;
-                    configureFormat(format);
-                }
-                if (long_flag.equals("--reps")) {
-                    String num_Reps = flagged_argument;
-                    setNumReps(num_Reps);
-                }
+    private void parseOptionalArguments() {
+        for (int i = 1; i < arguments.size(); i++) {
+            String arg = arguments.get(i);
+            if (arg.startsWith("--")) {
+                i = parseLongFlags(i, arg.substring(2));
+            } else if (arg.startsWith("-")) {
+                i = parseShortFlags(i, arg.substring(1));
+            } else {
+                throw new IllegalArgumentException("Error: Unexpected argument " + arg);
             }
-
-            if (arguments.get(i).startsWith("-")) {
-                String short_flag = arguments.get(i);
-                char[] short_flag_list = short_flag.toCharArray();
-
-                for (int j = 1; j < short_flag_list.length; j++) {
-                    if (short_flag_list[j] == 'a') {
-                        String apportionmentName = flagged_argument;
-                        configureStrategy(apportionmentName);
-                        i++;
-                    }
-                    if (short_flag_list[j] == 'f') {
-                        String format = flagged_argument;
-                        configureFormat(format);
-                        i++;
-                    }
-                    if (short_flag_list[j] == 'r') {
-                        String num_Reps = flagged_argument;
-                        setNumReps(num_Reps);
-                        i++;
-                    }
-                }
-            }
-
         }
     }
 
-    private int setNumReps(String arg) {
-        int numReps = Integer.parseInt(arg);
-        return numReps;
+    private int parseLongFlags(int index, String flag) {
+        switch (flag) {
+            case "reps" -> index = setNumReps(index);
+            case "format" -> index = configureFormat(index);
+            case "algorithm" -> index = configureStrategy(index);
+            default -> throw new IllegalArgumentException("Error: Invalid argument --" + flag);
+        }
+        return index;
+    }
 
+    private int parseShortFlags(int index, String flags) {
+        for (int i = 0; i < flags.length(); i++) {
+            char c = flags.charAt(i);
+            switch (c) {
+                case 'r' -> index = setNumReps(index);
+                case 'f' -> index = configureFormat(index);
+                case 'a' -> index = configureStrategy(index);
+                default -> throw new IllegalArgumentException("Error: Invalid argument -" + c);
+            }
+        }
+        return index;
+    }
+
+    private int setNumReps(int index) {
+        String arg = arguments.get(index + 1);
+        try {
+            int numReps = Integer.parseInt(arg);
+            if (numReps <= 0) {
+                throw new IllegalArgumentException("Error: Number of Reps Argument Must Be Positive");
+            }
+            config.setRepresentatives(numReps);
+            return index + 1;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Error: Positive integer expected for argument " + arg, e);
+        }
+    }
+
+    private int configureStrategy(int index) {
+        if (index + 1 >= arguments.size()) {
+            throw new IllegalArgumentException("Error: Missing argument for -a/--algorithm flag");
+        }
+        String apportionmentName = arguments.get(index + 1);
+        ApportionmentStrategyFactory factory = new ApportionmentStrategyFactory();
+        ApportionmentStrategy strategy = factory.getApportionmentStrategy(apportionmentName);
+        config.setApportionmentStrategy(strategy);
+        return index + 1;
+    }
+
+    private int configureFormat(int index) {
+        if (index + 1 >= arguments.size()) {
+            throw new IllegalArgumentException("Error: Missing argument for -f/--format flag");
+        }
+        String format = arguments.get(index + 1);
+        ApportionmentFormatFactory factory = new ApportionmentFormatFactory();
+        ApportionmentFormat apportionmentFormat = factory.getApportionmentFormat(format);
+        config.setApportionmentFormat(apportionmentFormat);
+        return index + 1;
     }
 
     private void configureStateReader() {
@@ -92,16 +110,5 @@ public class ArgumentsHandler {
         StateReaderFactory factory = new StateReaderFactory();
         StateReader stateReader = factory.getStateReader(filename);
         config.setStateReader(stateReader);
-    }
-
-    private void configureStrategy(String apportionmentName) {
-        ApportionmentStrategyFactory factory = new ApportionmentStrategyFactory();
-        ApportionmentStrategy strategy = factory.getApportionmentStrategy(apportionmentName);
-        config.setApportionmentStrategy(strategy);
-    }
-    private void configureFormat(String format) {
-        ApportionmentFormatFactory factory = new ApportionmentFormatFactory();
-        ApportionmentFormat apportionmentFormat = factory.getApportionmentFormat(format);
-        config.setApportionmentFormat(apportionmentFormat);
     }
 }
